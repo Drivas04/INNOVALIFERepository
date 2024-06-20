cd34965e4f5b77907af28c35a042272474523a2a
Drivas04 <drivasb@unbosque.edu.co>
2024-06-20 10:49:16 -0500

    Creación del backend de la aplicación 20/06

---
+++ BACKENDAPP/src/test/java/com/construdiseno/BackendappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.construdiseno;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
